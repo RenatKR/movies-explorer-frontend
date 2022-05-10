250367382219860c5f5f821fc57c2a06ff063506
RenatKR <kurbangaliev1987@mail.ru>
2022-05-10 22:37:43 +0300

    10052022

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './Promo.css'
 
 function Promo() {
     return (
         <section className="promo">
-            <h1 className="promo__title">Учебный&nbsp;проект студента факультета&nbsp;Веб&#8209;разработки</h1>
+            <h1 className="promo__title">Учебный&nbsp;проект студента факультета&nbsp;Веб&#8209;разработки.</h1>
         </section>
     );
 }
