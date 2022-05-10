4e0cb5a4a9dae261563c82a09d653439e56a5ebb
RenatKR <kurbangaliev1987@mail.ru>
2022-05-10 18:35:32 +0300

    10052022

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import profile from '../../images/profile.png';
+import './Navigation.css';
+import close from '../../images/close.svg';
 
 function Navigation() {
     return (
         <>
-            <div className="navigation__container">
-                <button className="navigation__close"></button>
-                <Link to='/' className="navigation__link">Главная</Link>
-                <Link to='/movies' className="navigation__link">Фильмы</Link>
-                <Link to='/saved-movies' className="navigation__link">Сохраненные фильмы</Link>
-                <div className="header__block">
-                    <Link className="header__link_color_black">Аккаунт</Link>
-                    <img className="header__img" src={profile} />
+            <div className="navigation">
+                <div className="navigation__container">
+                    <button className="navigation__close">
+                        <img src={close} />
+                    </button>
+                    <div className="navigation__wrapper" >
+                        <Link to='/' className="navigation__link_type_inner">Главная</Link>
+                        <Link to='/movies' className="navigation__link_type_inner">Фильмы</Link>
+                        <Link to='/saved-movies' className="navigation__link_type_inner">Сохраненные фильмы</Link>
+                    </div>
+                    <div className="navigation__block">
+                        <Link className="navigation__link_type_edit">Аккаунт</Link>
+                        <img className="navigation__img" src={profile} />
+                    </div>
                 </div>
             </div>
         </>
