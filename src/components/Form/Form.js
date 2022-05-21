16473df79e56651b385e36df5ee81d829e992a71
RenatKR <kurbangaliev1987@mail.ru>
2022-05-21 19:43:52 +0300

    21052022

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -39,46 +39,44 @@ function Form({
         <fieldset className='form__fieldset'>
           {signup && <label className='form__label'>Имя</label>}
           {signup && <input
-          {...register('name', {
-            required: true,
-          })}
+            type='text'
+            name='name'
+            className='form__input ${name}__input_type_name'
+            autoComplete='off'
+            {...register('name', {
+              required: "Поле обязательно к заполнению",
+              minLength: {
+                value: 2,
+                message: "Поле 'Имя' должно быть не менее 2 букв"
+              },
+              maxLength: {
+                value: 10,
+                message: "Поле 'Имя' должно быть не более 10 букв"
+              },
+              pattern: {
+                value: /[А-ЯЁA-Z\-\s]{2,10}$/i,
+                message: "Поле 'Имя' должно содержать только латиницу, кириллицу, пробел или дефис"
+              },
+            })}>
+          </input>}
+          <div>{errors.name && <p className='form__errors'>{errors.name.message || 'Ошибка!'}</p>}</div>
 
-            // // name='name'
-            // type='text'
-            // className='form__input ${name}__input_type_name'
-            // autoComplete='off'
-            // {...register('name', {
-            //   ...validators,
-            //   minLength: {
-            //     value: 2,
-            //     message: 'Не менее двух букв'
-            //   },
-            //   maxLength: {
-            //     value: 10,
-            //     message: 'Не более десяти букв'
-            //   },
-            //   pattern: {
-            //     value: /[А-ЯЁ]{2,10}/i,
-            //     message: 'Только киррилица'
-            //   },
-            //   requires: true,
-            // })}
-          // onChange={handleChangeName}
-          // minLength='2'
-          // maxLength='30'
-          ></input>}
-          <div>{errors.name && <p className='form__errors'>Error!</p>}</div>
-          {/* <p className='form__errors'>{errors.name}</p> */}
-          {/* <label className='form__label'>Email</label>
+          <label className='form__label'>Email</label>
           <input
             name='email'
             type='email'
-            pattern=''
             className='form__input ${name}__input_type_email'
             autoComplete='off'
+            {...register('name', {
+              required: "Поле обязательно к заполнению",
+              pattern: {
+                value: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i,
+                message: "Поле 'Имя' должно содержать только латиницу, кириллицу, пробел или дефис"
+              },
+            })}
           // onChange={handleChangeEmail}
           ></input>
-          <p className='form__errors'>{errors.email}</p>
+          <div>{errors.email && <p className='form__errors'>{errors.email.message || 'Ошибка!'}</p>}</div>
           <label className='form__label'>Пароль</label>
           <input
             name='password'
@@ -88,7 +86,7 @@ function Form({
             autoComplete='off'
           // onChange={handleChangePassword}
           ></input>
-          <p className='form__errors'>{errors.password}</p> */}
+          <p className='form__errors'>{errors.password}</p>
 
           <button type='submit' className='form__button'>{buttonText}</button>
 
